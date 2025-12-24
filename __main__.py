03f7ad7a6479f4f1ffb5a6f68eb6bd1f547a8a63
Igor Amirkhanian <ammigo403@gmail.com>
2025-12-24 11:31:16 +0400

    Removed the duplicate class

--- __main__.py
+++ __main__.py
@@ -4,25 +4,6 @@ from trade_engine.domain.wallet import Wallet
 from trade_engine.services.trade_service import TradeService
 
 def main():
-    user = User(1, "Alice")
-    wallet = Wallet(user.id)
-
-    trade = TradeService()
-
-    trade.buy(wallet, 100)
-    trade.sell(wallet, 30)
-    trade.sell(wallet, 100)  # недостаточно средств
-
-    trade.show_history(user.id)
-
-if __name__ == "__main__":
-    main()
-
-from trade_engine.domain.user import User
-from trade_engine.domain.wallet import Wallet
-from trade_engine.services.trade_service import TradeService
-
-def main():
     # создаем пользователя и кошелек
     user = User(1, "Alice")
     wallet = Wallet(user.id)
