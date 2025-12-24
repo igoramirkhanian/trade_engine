7cf9465e68412fab2801c3aaa83dd808e1b83d9d
Igor Amirkhanian <ammigo403@gmail.com>
2025-12-24 12:20:24 +0400

    add version module and log version on startup

--- __main__.py
+++ __main__.py
@@ -1,9 +1,20 @@
 
+import logging
+from trade_engine.version import __version__  # версия приложения
+
 from trade_engine.domain.user import User
 from trade_engine.domain.wallet import Wallet
 from trade_engine.services.trade_service import TradeService
 
+
 def main():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s | %(levelname)s | %(message)s"
+    )
+
+    logging.info(f"Trade Engine starting, version {__version__}")
+
     # создаем пользователя и кошелек
     user = User(1, "Alice")
     wallet = Wallet(user.id)
@@ -14,6 +25,7 @@ def main():
     trade.buy(wallet, 100)
     trade.sell(wallet, 30)
     trade.sell(wallet, 100)
+
 
 if __name__ == "__main__":
     main()
